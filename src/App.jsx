c7cad6a3ecc4fae83e9f4fb256ebc07771046300
Asif <msasifiiuc@gmail.com>
2023-09-13 07:17:38 +0600

    data added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Blogs />
+    </>
+  )
+}
+
+export default App
